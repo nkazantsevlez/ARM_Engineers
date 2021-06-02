24977ec17785339e125527dfd8553b9146160edc
Nikita Kazantsev <nkazantsevlez@gmail.com>
2021-06-02 12:47:32 +0800

    Include static class User

---
+++ ARM_Engineers/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARM_Engineers
+{
+    static class User
+    {
+        static int ID;
+        static String Surname;
+        static String Name;
+    }
+}
