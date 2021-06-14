c91055fd2b6b06cd0192e9597207e2e438ff71eb
Nikita Kazantsev <nkazantsevlez@gmail.com>
2021-06-15 00:11:42 +0800

    Added reports and implemented the logic of the Report_Window

--- ARM_Engineers/Report_Window.xaml.cs
+++ ARM_Engineers/Report_Window.xaml.cs
@@ -22,6 +22,10 @@ namespace ARM_Engineers
         public Report_Window()
         {
             InitializeComponent();
+            /*FirstReport firstReport = new FirstReport();
+            firstReport.Load(@"FirstReport.rep");
+            Event_Log_ReportViewer.ViewerCore.ReportSource = firstReport;*/
+
         }
 
         private void Close_Button_Click(object sender, RoutedEventArgs e)
@@ -32,6 +36,23 @@ namespace ARM_Engineers
         private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
         {
             DragMove();
+        }
+
+        private void Selected_Report_Combobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch(Selected_Report_Combobox.SelectedIndex)
+            {
+                case 0: All_Devices_Report all_Devices_Report = new All_Devices_Report();
+                    all_Devices_Report.Load(@"All_Devices_Report.rep");
+                    View_ReportViewer.ViewerCore.ReportSource = all_Devices_Report;
+                    break;
+                case 1: 
+                    Event_Log_Report event_Log_Report = new Event_Log_Report();
+                    event_Log_Report.Load(@"Event_Log_Report.rep");
+                    View_ReportViewer.ViewerCore.ReportSource = event_Log_Report; 
+                    break;
+                default: MessageBox.Show("Ошибка!");break;
+            }
         }
     }
 }
