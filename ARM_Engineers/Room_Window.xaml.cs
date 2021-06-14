15265adb5ae4dd5932c8ae84786dab5304d9aeac
Nikita Kazantsev <nkazantsevlez@gmail.com>
2021-06-14 12:57:19 +0800

    Create Room_Window

---
+++ ARM_Engineers/Room_Window.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ARM_Engineers
+{
+    /// <summary>
+    /// Логика взаимодействия для Room_Window.xaml
+    /// </summary>
+    public partial class Room_Window : Window
+    {
+        arm_engineersEntities context = new arm_engineersEntities();
+        public Room_Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Close_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
